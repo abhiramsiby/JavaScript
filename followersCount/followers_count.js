d4230d0b1f3a663aae8bfdacd602182476205c8a
Abhiram Siby <abhiramsibi2@gmail.com>
2026-06-11 11:48:39 -0400

    Added a reset button for the followercount

--- followersCount/followers_count.js
+++ followersCount/followers_count.js
@@ -10,10 +10,16 @@ function displayCount(){
 }
 
 function checkCountValue(){
-    if(count==20){
+    if(count==10){
         alert("Your Instagram post gained 10 followers! Congratulations!");
     }
     else if(count==20){
         alert("Your Instagram post gained 20 followers! Keep it!");
     }
+}
+
+function resetCount(){
+    count=0;
+    displayCount();
+    alert("Yours Followers Count has been Reset!");
 }
